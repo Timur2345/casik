acd1ad82018c91429436f55791128564a2edd999
naumovT <tima.naumov09@mail.ru>
2025-03-09 17:16:10 +0300

    [e[e

--- End/index.js
+++ End/index.js
@@ -3,6 +3,12 @@ let enter_button = document.querySelector(".enter-button")
 let balances = document.querySelectorAll(".balance-amount")
 let balance = +balances[0].innerHTML
 let vivod = document.querySelector(".b-2")
+let price_1 = document.querySelector(".price-1")
+let price_2 = document.querySelector(".price-2")
+let price_3 = document.querySelector(".price-3")
+let price_4 = document.querySelector(".price-4")
+let price_5 = document.querySelector(".price-5")
+let price_6 = document.querySelector(".price-6")
 
 button.addEventListener("click", function () {
   if (balance > 0){
@@ -10,10 +16,34 @@ button.addEventListener("click", function () {
       for (let i = 0; i < balances.length; i++) {
         balances[i].innerHTML = balance
       }
-      vivod.innerHTML = +vivod.innerHTML - 100
+      vivod.innerHTML = +vivod.innerHTML + 100
   }
 })
 
 enter_button.addEventListener("click", function () {
     window.open("./Images/picture.png")
+})
+
+price_1.addEventListener("click", function () {
+  window.open("./Images/picture.png")
+})
+
+price_2.addEventListener("click", function () {
+  window.open("./Images/picture.png")
+})
+
+price_3.addEventListener("click", function () {
+  window.open("./Images/picture.png")
+})
+
+price_4.addEventListener("click", function () {
+  window.open("./Images/picture.png")
+})
+
+price_5.addEventListener("click", function () {
+  window.open("./Images/picture.png")
+})
+
+price_6.addEventListener("click", function () {
+  window.open("./Images/picture.png")
 })
